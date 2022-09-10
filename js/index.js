b457ecb8595b096ad7f500adc2f7a6235b9683a0
iamsagnik <sagnikmitra008@gmail.com>
2022-09-10 15:25:16 +0530

    More HTML Tags

--- js/index.js
+++ js/index.js
@@ -1,9 +1,30 @@
 let root = document.querySelector('#root');
+console.log(root);
+
+let box = document.createElement('input');
+root.append(box);
+
+// function to add element in page
+let addElement = (el, cl, tx) => {
+  let e = document.createElement(el);
+  e.classList.add(cl);
+  e.textContent = tx;
+  root.append(e);
+}
+
+addElement('button', 'button-blue', 'Square');
+addElement('button', 'button-orange', 'Sqr Root');
+
+// fetch and api
+
 
 let h = document.createElement('h3');
 h.textContent = "Welcome";
 
 root.append(h);
+
+// DRY
+// Don't Repeat Yourself
 
 let fruits = [
   'mango',
