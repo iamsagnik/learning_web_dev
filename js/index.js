ad9a4487a2c3069d507b2b06f6f3d420ff183d73
iamsagnik <sagnikmitra008@gmail.com>
2022-09-10 14:59:15 +0530

    Learning more about js and html

--- js/index.js
+++ js/index.js
@@ -1,38 +1,25 @@
-let a = 5;
-let b = 3.7;
+let root = document.querySelector('#root');
 
-const pi = 3.1428;
-const c = 2.9782 * (10**8);
+let h = document.createElement('h3');
+h.textContent = "Welcome";
 
-let userName = 'anupam';
+root.append(h);
 
-// arrays
-let cars = [ 'punch', 'alto', 'creta', 'tavera']
+let fruits = [
+  'mango',
+  'apple',
+  'banana',
+  'grapes',
+  'orange'
+]
 
-for (let i=0; i<cars.length; i++) {
-  let u = document.querySelector('#myUl');
+
+let u = document.createElement('ul');
+
+for (let n = 0; n<fruits.length; n++) {
   let l = document.createElement('li');
-  l.innerText = cars[i].toUpperCase();
+  l.textContent = fruits[n];
   u.append(l);
 }
 
-// Objects 
-// this syntax is also known as JSON
-// JSON stands for Javascript Object Notation
-// it is very popular among developers 
-// mostly used for data transfer
-const user_1 = {
-  // key : value
-  "name": "Sagnik",
-  "age": 19,
-  "cars_owning": cars
-}
-// in python it is called Dictionary
-b = 3.8;
-console.log(b);
-
-
-let el = document.querySelector("body");
- el.children[3].remove();
- el.children[3].remove();
- console.log(el.children[3]);
+root.append(u);
